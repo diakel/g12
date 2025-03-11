9b70f059a1a5989dd926aebaaa670ae3bc737526
Mandy Deng <miaodan@students.cs.ubc.ca>
2025-03-11 00:13:34 -0700

    started maps, added map of states only for now

--- js/main.js
+++ js/main.js
@@ -5,4 +5,30 @@ const parseMonthYear = d3.timeParse('%b.%y');
 
 d3.json('data/books_hierarchy_subset.json').then((subjectsHierarchyData) => {
   const sunburst = new Sunburst({parentElement: '#vis-sunburst'}, subjectsHierarchyData);
-});
+}); 
+
+
+/**
+ * Load geo data
+ */
+
+d3.json('data/us-states.json')
+  .then(data => {
+    // Mercator projection
+    const geoMap1 = new GeoMap({ 
+      parentElement: '#mercator',
+      projection: d3.geoMercator()
+    }, data);
+
+    // Lambert conformal conic projection
+    // See: https://observablehq.com/@bryik/statscans-most-common-map-projection
+    // We need to rotate the globe. You can often find specifications for popular projections
+    // and world regions somewhere on the internet or you tweak the parameters to get a satisfying result.
+    const geoMap2 = new GeoMap({ 
+      parentElement: '#lambert',
+      projection: d3.geoConicConformal()
+          .parallels([49, 77])
+          .rotate([91.86667, 0])
+    }, data);
+  })
+  .catch(error => console.error(error));
